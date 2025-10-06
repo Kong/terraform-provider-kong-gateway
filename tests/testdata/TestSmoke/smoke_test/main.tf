2992f307b3e2c5354b56c5d2e8e4d341d20dec44
Michael Heap <m@michaelheap.com>
2025-10-06 13:23:18 +0100

    Start plugin tests

--- tests/testdata/TestSmoke/smoke_test/main.tf
+++ tests/testdata/TestSmoke/smoke_test/main.tf
@@ -37,16 +37,6 @@ resource "kong-gateway_route" "hello" {
   }
 }
 
-#resource "kong-gateway_plugin_basic_auth" "basic_auth" {
-#  enabled = true
-#  service = {
-#    id = kong-gateway_service.httpbin.id
-#  }
-#  config = {
-#    hide_credentials = false
-#  }
-#}
-#
 resource "kong-gateway_consumer" "alice" {
   username  = "alice"
   custom_id = "alice"

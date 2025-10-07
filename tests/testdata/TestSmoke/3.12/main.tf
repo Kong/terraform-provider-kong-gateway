40597e597b5773d523ab76574c574f25459eb756
Michael Heap <m@michaelheap.com>
2025-10-07 12:02:18 +0100

    Remove commented out items

--- tests/testdata/TestSmoke/3.12/main.tf
+++ tests/testdata/TestSmoke/3.12/main.tf
@@ -197,22 +197,3 @@ resource "kong-gateway_vault" "my_vault" {
     base64_decode = false
   })
 }
-
-
-
-
-#
-#resource "kong-gateway_plugin_rate_limiting" "my_rate_limiting_plugin" {
-#  enabled = true
-#  config = {
-#    minute = 5
-#    policy = "local"
-#  }
-#
-#  protocols = ["http", "https"]
-#  route = {
-#    id = kong-gateway_route.hello.id
-#  }
-#}
-#
-#

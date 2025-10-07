3cfa17e9e157e24b92cb9eb209a17cb80ff1058e
Michael Heap <m@michaelheap.com>
2025-10-07 11:26:56 +0100

    3.12 tests

--- tests/smoke_test.go
+++ tests/smoke_test.go
@@ -1,22 +1,56 @@
 package tests
 
 import (
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/config"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
+type TestCase struct {
+	name    string
+	runWhen *string
+}
+
 func TestSmoke(t *testing.T) {
 	t.Parallel()
-	t.Run("smoke test", func(t *testing.T) {
-		resource.Test(t, resource.TestCase{
-			Steps: []resource.TestStep{
-				{
-					ProtoV6ProviderFactories: providerFactory,
-					ConfigDirectory:          config.TestNameDirectory(),
+
+	var testCases = []TestCase{
+		//{
+		//	name:    "3.10",
+		//	runWhen: ptr("3.10"),
+		//},
+		//{
+		//	name:    "3.11",
+		//	runWhen: ptr("3.11"),
+		//},
+		{
+			name:    "3.12",
+			runWhen: ptr("3.12"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			currentVersion := os.Getenv("KONG_VERSION")
+			if tc.runWhen != nil && *tc.runWhen != currentVersion {
+				t.Skip("Skipping " + tc.name + " (" + *tc.runWhen + ") because KONG_VERSION is " + currentVersion)
+			}
+
+			resource.Test(t, resource.TestCase{
+				Steps: []resource.TestStep{
+					{
+						ProtoV6ProviderFactories: providerFactory,
+						ConfigDirectory:          config.TestNameDirectory(),
+					},
 				},
-			},
+			})
 		})
-	})
+	}
+}
+
+func ptr(s string) *string {
+	return &s
 }

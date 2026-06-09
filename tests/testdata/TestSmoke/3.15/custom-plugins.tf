8c8eca856580d0c11ebeea2d356bcf5035a68e0c
Harsha Dixit <harsha.dixit@konghq.com>
2026-06-09 15:07:56 +0530

    tests: add 3.15 tests including cloned plugins and custom plugin streaming

---
+++ tests/testdata/TestSmoke/3.15/custom-plugins.tf
@@ -0,0 +1,55 @@
+resource "kong-gateway_custom_plugin_streaming" "my_streaming_plugin" {
+  name = "my-custom-streaming"
+
+  handler = <<-EOT
+    return {
+      VERSION = "1.0,0",
+      PRIORITY = 500,
+      access = function(self, config)
+        kong.service.request.set_header(config.name, config.value)
+      end
+    }
+  EOT
+
+  schema = <<-EOT
+    return {
+      name = "set-header",
+      fields = {
+        { protocols = require("kong.db.schema.typedefs").protocols_http },
+        {
+          config = {
+            type = "record",
+            fields = {
+              { name = { description = "The name of the header to set.", type = "string", required = true, }, },
+              { value = { description = "The value for the header.", type = "string", required = true, }, },
+            },
+          },
+        },
+      },
+    }
+  EOT
+}
+
+
+resource "kong-gateway_custom_plugin" "seth1" {
+  name = "set-header"
+  instance_name = "setheader-instance"
+  config = {
+    name = "x-custom-header"
+    value = "my-custom-value"
+  }
+}
+
+
+resource "kong-gateway_cloned_plugin" "my_cloned_plugin" {
+  name = "custom-acl"
+  ref  = "acl"
+}
+
+resource "kong-gateway_custom_plugin" "my_custom_acl" {
+  group       = "demo"
+  name = "custom-acl"
+  config = {
+    allow = ["mygroup"]
+  }
+}
